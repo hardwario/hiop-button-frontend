2e0fbd149d49c915294a3d47aeb9c087883a421e
Karel Blavka <karel.blavka@hardwario.com>
2020-03-20 16:38:50 +0100

    Add axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import Buefy from 'buefy'
-import './assets/scss/app.scss'
+import Vue from 'vue';
+import App from './App.vue';
+import Buefy from 'buefy';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
-Vue.use(Buefy)
+import './assets/scss/app.scss';
 
-Vue.config.productionTip = false
+Vue.use(Buefy);
+Vue.use(VueAxios, axios);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
